372ea44d1f84d18b75e5f955d3f2e97904721dd3
Morvin-Ian <morvinian@gmail.com>
2022-09-21 06:08:26 +0300

    minor edits

--- credentials.py
+++ credentials.py
@@ -1,7 +1,8 @@
-consumer_key = 'IDsfx5wReFDVCB7i6e6c8McwQcen7gEd'
-consumer_secrete = '3T5OLpsZlRAZmfVt'
-lnm_passkey ="bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
-bs_shortcode ="174379" # paybill number
+# Provided by Safaricom
+consumer_key = ''
+consumer_secrete = ''
+lnm_passkey =""
+bs_shortcode ="" # paybill number
 acess_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
 b2c_initiator = '' # initiator name
 
